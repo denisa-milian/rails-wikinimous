a1521f61e67bcbadfa67dea6faf419d2de4e3623
Denisa Milian <denisa.g.milian@gmail.com>
2020-08-11 16:11:51 +0100

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  article = Article.new(title: Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 4), content: Faker::Movies::VForVendetta.quote)
+  article.save!
+end
